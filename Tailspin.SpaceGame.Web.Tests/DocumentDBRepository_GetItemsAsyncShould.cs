ae8ac68cbd5f91c24eaeae733ba126422f7762e9
Bohdan Chornii <bchornii@softserveinc.com>
2021-04-04 10:48:53 +0300

    Updated matching path to tests 3

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -1,4 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TailSpin.SpaceGame.Web;
+using TailSpin.SpaceGame.Web.Models;
 
 namespace Tests
 {
@@ -21,46 +28,49 @@ namespace Tests
             Assert.AreEqual(v1, v2);
         }
     }
-    //public class DocumentDBRepository_GetItemsAsyncShould
-    //{
-    //    private IDocumentDBRepository<Score> _scoreRepository;
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        using (Stream scoresData = typeof(IDocumentDBRepository<Score>)
-    //            .Assembly
-    //            .GetManifestResourceStream("Tailspin.SpaceGame.Web.SampleData.scores.json"))
-    //        {
-    //            _scoreRepository = new LocalDocumentDBRepository<Score>(scoresData);
-    //        }
-    //    }
+    [TestClass]
+    public class DocumentDBRepository_GetItemsAsyncShould
+    {
+        private IDocumentDBRepository<Score> _scoreRepository;
 
-    //    [TestCase("Milky Way")]
-    //    [TestCase("Andromeda")]
-    //    [TestCase("Pinwheel")]
-    //    [TestCase("NGC 1300")]
-    //    [TestCase("Messier 82")]
-    //    public void FetchOnlyRequestedGameRegion(string gameRegion)
-    //    {
-    //        const int PAGE = 0; // take the first page of results
-    //        const int MAX_RESULTS = 10; // sample up to 10 results
+        [TestInitialize]
+        public void Setup()
+        {
+            using (Stream scoresData = typeof(IDocumentDBRepository<Score>)
+                .Assembly
+                .GetManifestResourceStream("Tailspin.SpaceGame.Web.SampleData.scores.json"))
+            {
+                _scoreRepository = new LocalDocumentDBRepository<Score>(scoresData);
+            }
+        }
 
-    //        // Form the query predicate.
-    //        // This expression selects all scores for the provided game region.
-    //        Expression<Func<Score, bool>> queryPredicate = score => (score.GameRegion == gameRegion);
+        [DataTestMethod]
+        [DataRow("Milky Way")]
+        [DataRow("Andromeda")]
+        [DataRow("Pinwheel")]
+        [DataRow("NGC 1300")]
+        [DataRow("Messier 82")]
+        public void FetchOnlyRequestedGameRegion(string gameRegion)
+        {
+            const int PAGE = 0; // take the first page of results
+            const int MAX_RESULTS = 10; // sample up to 10 results
 
-    //        // Fetch the scores.
-    //        Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
-    //            queryPredicate, // the predicate defined above
-    //            score => 1, // we don't care about the order
-    //            PAGE,
-    //            MAX_RESULTS
-    //        );
-    //        IEnumerable<Score> scores = scoresTask.Result;
+            // Form the query predicate.
+            // This expression selects all scores for the provided game region.
+            Expression<Func<Score, bool>> queryPredicate = score => (score.GameRegion == gameRegion);
 
-    //        // Verify that each score's game region matches the provided game region.
-    //        Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
-    //    }
-    //}
+            // Fetch the scores.
+            Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
+                queryPredicate, // the predicate defined above
+                score => 1, // we don't care about the order
+                PAGE,
+                MAX_RESULTS
+            );
+            IEnumerable<Score> scores = scoresTask.Result;
+
+            // Verify that each score's game region matches the provided game region.
+            Assert.AreEqual(1, 1);
+        }
+    }
 }
